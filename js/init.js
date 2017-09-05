9042dcec4fa482c43139c49d3a08dfbe0e6cc84a
GRAZVYDAS RAMANAUSKAS <ramanauskasgrazvydas@gmail.com>
2017-09-05 21:32:39 +0300

    gr

--- js/init.js
+++ js/init.js
@@ -6,3 +6,16 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+$('.carousel.carousel-slider').carousel({fullWidth: true});
+
+var data = {
+  series: [5, 3, 4]
+};
+
+var sum = function(a, b) { return a + b };
+
+new Chartist.Pie('.ct-chart', data, {
+  labelInterpolationFnc: function(value) {
+    return Math.round(value / data.series.reduce(sum) * 100) + '%';
+  }
+});
